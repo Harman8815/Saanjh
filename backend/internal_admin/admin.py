168c0a51dd6215f7eac706a72b76c89c6efbfae8
Harman Deep Singh <harman88157@gmail.com>
2026-04-24 14:58:10 +0530

    feat: register Wedding models in admin
    - Register Wedding, WeddingStatus, VenueCatalog, Venue models
    - Add list_display, list_filter, search_fields for Wedding
    - Add date_hierarchy for wedding timeline navigation
    - Implement permission controls for wedding management
    - Restrict venue catalog management to superuser only

--- backend/internal_admin/admin.py
+++ backend/internal_admin/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from accounts.models import User, Role, Settings
+from weddings.models import Wedding, WeddingStatus, VenueCatalog, Venue
 
 
 @admin.register(User)
@@ -77,3 +78,83 @@ class SettingsAdmin(admin.ModelAdmin):
 
     def has_delete_permission(self, request, obj=None):
         return request.user.is_superuser
+
+
+@admin.register(Wedding)
+class WeddingAdmin(admin.ModelAdmin):
+    """Admin configuration for Wedding model"""
+    list_display = ['id', 'user', 'bride_name', 'groom_name', 'wedding_date', 'status']
+    list_filter = ['status', 'wedding_date']
+    search_fields = ['bride_name', 'groom_name', 'user__username', 'user__email']
+    ordering = ['-wedding_date']
+    readonly_fields = ['created_at', 'updated_at']
+    date_hierarchy = 'wedding_date'
+    list_per_page = 25
+
+    def has_view_permission(self, request, obj=None):
+        return request.user.is_staff
+
+    def has_add_permission(self, request):
+        return request.user.is_staff
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_staff
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+
+@admin.register(WeddingStatus)
+class WeddingStatusAdmin(admin.ModelAdmin):
+    """Admin configuration for WeddingStatus model"""
+    list_display = ['id', 'name']
+    search_fields = ['name']
+
+    def has_add_permission(self, request):
+        return request.user.is_superuser
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+
+@admin.register(VenueCatalog)
+class VenueCatalogAdmin(admin.ModelAdmin):
+    """Admin configuration for VenueCatalog model"""
+    list_display = ['id', 'name', 'type', 'capacity_min', 'capacity_max', 'price', 'rating']
+    list_filter = ['type']
+    search_fields = ['name', 'address']
+    ordering = ['name']
+
+    def has_view_permission(self, request, obj=None):
+        return request.user.is_staff
+
+    def has_add_permission(self, request):
+        return request.user.is_superuser
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+    """Admin configuration for Venue model"""
+    list_display = ['id', 'venue_catalog']
+    search_fields = ['venue_catalog__name']
+
+    def has_view_permission(self, request, obj=None):
+        return request.user.is_staff
+
+    def has_add_permission(self, request):
+        return request.user.is_staff
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_staff
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_staff
